df8823aa7fe5968a5d23dddeb6f2a35e3cbf6bf3
majkii2115 <szymbaramichal@gmail.com>
2022-04-23 19:20:34 +0200

    Initial commit

---
+++ TodoAPI/Configure.AppHost.cs
@@ -0,0 +1,25 @@
+using Funq;
+using ServiceStack;
+using TodoAPI.ServiceInterface;
+
+[assembly: HostingStartup(typeof(TodoAPI.AppHost))]
+
+namespace TodoAPI;
+
+public class AppHost : AppHostBase, IHostingStartup
+{
+    public void Configure(IWebHostBuilder builder) => builder
+        .ConfigureServices(services => {
+            // Configure ASP.NET Core IOC Dependencies
+        });
+
+    public AppHost() : base("TodoAPI", typeof(MyServices).Assembly) {}
+
+    public override void Configure(Container container)
+    {
+        // Configure ServiceStack only IOC, Config & Plugins
+        SetConfig(new HostConfig {
+            UseSameSiteCookies = true,
+        });
+    }
+}
